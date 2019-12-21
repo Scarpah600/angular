4fb95d2d3b173042dfae81a04e52dc57138c0f85
Scarpah600 <gustavo.scarpin.2020@gmail.com>
2019-12-21 13:13:57 -0300

    atualizando login

--- src/app/services/domain/auth.service.ts
+++ src/app/services/domain/auth.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { CredenciaisDTO } from "../../../models/credencias.dto";
 import { API_CONFIG } from "../../../config/api.config";
+import { LocalUser } from "../../../models/local_user";
+import { StorageService } from "../storage.service";
 
 
 @Injectable()
 export class AuthService {
 
-    constructor(public http: HttpClient) {
+    constructor(public http: HttpClient, public storage: StorageService) {
     }
 
     authenticate(creds : CredenciaisDTO) {
@@ -19,5 +21,15 @@ export class AuthService {
                 responseType: 'text'
 
             });
+    }
+    successfullLogin(authorizationValue : string){
+        let tok = authorizationValue;
+        let user : LocalUser = {
+            token: tok
+        };
+        this.storage.setLocalUser(user);
+    }
+    logout(){
+        this.storage.setLocalUser(null);
     }
 }
