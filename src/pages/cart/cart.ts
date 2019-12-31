a2fcddbe9ab565dd232d56a1e13fca1e5b683fe8
Scarpah600 <gustavo.scarpin.2020@gmail.com>
2019-12-31 12:43:17 -0300

    Terminando funcionalidade de carrinho de compra

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CartItem } from '../../models/cart-item';
 import { CartService } from '../../app/services/cart.service';
+import { ProdutoDTO } from '../../models/produto.dto';
 
 
 @IonicPage()
@@ -23,6 +24,19 @@ export class CartPage {
     this.items = cart.items;
   }
 
-  
-
+  removeItem(produto: ProdutoDTO){
+    this.items = this.cartservice.removeProduto(produto).items;
+  }
+  increaseQuantity(produto : ProdutoDTO){
+    this.items = this.cartservice.increaseQuantity(produto).items;
+  }
+  decreaseQuanty(produto : ProdutoDTO){
+    this.items = this.cartservice.decreaseQuantity(produto).items;
+  }
+  total(): number{
+    return this.cartservice.total();
+  }
+  goOn(){
+    this.navCtrl.setRoot('CategoriasPage');
+  }
 }
