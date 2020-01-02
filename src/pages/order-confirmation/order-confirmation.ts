b9a4398aaa4585712f853f7a9f593e08e64c1fc0
Scarpah600 <gustavo.scarpin.2020@gmail.com>
2020-01-02 15:30:49 -0300

    informando pedido registrado ao usuario

--- src/pages/order-confirmation/order-confirmation.ts
+++ src/pages/order-confirmation/order-confirmation.ts
@@ -19,6 +19,7 @@ export class OrderConfirmationPage {
   cartItems: CartItem[];
   cliente: ClienteDTO;
   endereco: EnderecoDTO;
+  codpedido: string;
 
   constructor(
     public navCtrl: NavController, 
@@ -51,20 +52,31 @@ export class OrderConfirmationPage {
   total() : number {
     return this.cartService.total();
   } 
-  voltar(){
+
+  back() {
     this.navCtrl.setRoot('CartPage');
   }
-  checkout(){
+
+  home() {
+    this.navCtrl.setRoot('CategoriasPage');
+  }
+
+  checkout() {
     this.pedidoService.insert(this.pedido)
-    .subscribe(response => {
+      .subscribe(response => {
         this.cartService.createOrClearCart();
-        console.log(response.headers.get('location'));
-    },
-    error => {
-      if(error.status == 403){
-        this.navCtrl.setRoot('HomePage');
-      }
-    })
+        this.codpedido = this.extractId(response.headers.get('location'));
+      },
+      error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+  }
+
+  private extractId(location : string) : string {
+    let position = location.lastIndexOf('/');
+    return location.substring(position + 1, location.length);
   }
 }
 
